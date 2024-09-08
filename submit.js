9c070fd893a79c79b855f4e62a595e2d366dfdce
akash jha <akashjhasciencea@gmail.com>
2024-09-08 23:35:58 +0530

    sends data to port 5000

---
+++ submit.js
@@ -0,0 +1,18 @@
+document.getElementById("form1").addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let form = document.getElementById("form1");
+    const username = form.firstname.value;
+    const password = form.lastname.value;
+
+    fetch("http://localhost:5000/register", {
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body:JSON.stringify({username,password}),
+  
+    })
+    
+})
+
+
